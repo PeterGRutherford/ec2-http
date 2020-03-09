fcac68c4514067e1fa8f5d45882eb784ce1e95cf
PeterGRutherford <54694350+PeterGRutherford@users.noreply.github.com>
2020-03-09 12:40:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
-<html>
+<html><head><title>tritech.io</title></head>
+<style>
+body{font-family: system-ui, sans-serif;}
+html{font-family: system-ui, sans-serif;}
+  
+pre{ 
+  background-color: #fdf6e3;color: #b58900;
+  border: solid 1px; border-radius: 20px; padding : 10px;
+}
+
+a{ 	background-color:#cb4b16; ;
+	color: #fff;
+	font-size:20px;
+	text-decoration:none;
+	border: solid 1px;
+	border-radius: 5px;
+	padding : 2px;
+	
+}
+
+a:hover{	background-color: #dc322f; ;
+		color:  #fdf6e3; ;
+		text-decoration:none;
+}
+</style>
 <body>
+<center>
+<a href = "10.183.2.11" class = "menu" >10.183.2.11</a>&nbsp; 
+
+<hr />
+OUTPUT<pre></pre><hr />
 </body>
 </html>
